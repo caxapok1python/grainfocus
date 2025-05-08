e85e115ccb7b4614e75c6d923765e91089738adf
Caxapok <caxapok.js@yandex.ru>
2025-05-08 16:06:11 +0300

    update port

--- web/app.py
+++ web/app.py
@@ -27,4 +27,4 @@ if __name__ == '__main__':
     # путь до файла БД (SQLite)
     db_path = os.path.join(app.root_path, '..', 'database.sqlite')
     os.environ.setdefault('SQLITE_PATH', db_path)
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
